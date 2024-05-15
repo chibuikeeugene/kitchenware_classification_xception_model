376af021bb615a7a4ffd3e5008395bc5bd7221f2
chibuikeeugene <chibuikeeugene@gmail.com>
2024-05-15 06:13:18 +0100

    completed a new improved research model with xception and improved val_accuracy to over 90 percent

--- fetch_data_from_kaggle.sh
+++ fetch_data_from_kaggle.sh
@@ -1,1 +1,6 @@
-data =  
+# ! ~/.local/bin/kaggle datasets download -d agrigorev/kitchenware-extra-images
+! ~/.local/bin/kaggle competitions download -c kitchenware-classification && \
+! mkdir  ./kitchenware-data && \
+! chmod +w ./kitchenware-data && \
+! unzip ./kitchenware-classification.zip -d ./kitchenware-data && \
+! rm -R ./kitchenware-classification.zip
